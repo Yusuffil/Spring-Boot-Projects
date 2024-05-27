45ac21239d306b3b64b74149c650222417f7b22b
Yusuffil <yusuffil50@gmail.com>
2024-05-27 13:15:17 +0300

    Third Project

---
+++ Library/src/main/java/yusuffil/Library/LibraryApplication.java
@@ -0,0 +1,17 @@
+package yusuffil.Library;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+
+		User.add();
+
+	}
+
+
+}
---
+++ Library/src/main/java/yusuffil/Library/User.java
@@ -0,0 +1,12 @@
+package yusuffil.Library;
+
+import lombok.Data;
+
+
+public class User {
+
+       public static void add(){
+        System.out.println("Sisteme eklendi");
+    }
+
+}
